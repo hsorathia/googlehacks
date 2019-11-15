c88091fe8797e7bc7b604cecd299483877f13e13
Glitch (hello-express) <none>
2019-11-15 18:24:59 +0000

    👬🐿 Checkpoint ./example.js:2827281/621

--- example.js
+++ example.js
@@ -2,20 +2,23 @@ module.exports = {
   createnewchannel: function(guild, json, link) {
     return new Promise ( (res,rej) => {
       console.log('Create new channel called');
-       //PLEASE CHANGE THESE LINES
       //Grabbing variables from json. Declaring these variables so you can easily manage them if the json setup is different, which it wil be
       var ch_name = json.Name;
       var ch_desc = json.Description;
       // var date = json.Date
       // var calendar_link = link;
-      var msg = "Date is " + j
+      var msg = "Date is " + json.Date.toString() + ".\n The link is "+ link.toString()
+      var event_id = '644959822910849025'
       
       guild
         .createChannel(ch_name, { type: "text" })
         .then(async txtChannel => {
         console.log(`text channel created`);
           await txtChannel.edit({ topic: ch_desc });
-          await txtChannel.send(calendar_link);
+          await txtChannel.setParent(event_id)
+              .then(updated => console.log(`Set the category of ${updated.name} to ${updated.parent.name}`))
+              .catch(console.error);
+          await txtChannel.send(msg);
           res(txtChannel)
         })
         .catch(console.error);
