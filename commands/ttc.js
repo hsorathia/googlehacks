b34881845ff4baa90b1a644014f756e6594b74e6
Glitch (hello-express) <none>
2019-11-15 19:00:48 +0000

    👘👨‍❤️‍👨 Checkpoint ./example.js:2827281/852 ./commands/ttc.js:2827281/169

--- commands/ttc.js
+++ commands/ttc.js
@@ -2,13 +2,13 @@ const example = require(`/app/example.js`);
 exports.run = async (bot, message, args) => {
   //if (!message.member.hasPermission("ADMINISTRATOR")) return;
   var json = {
-    Name: "game night",
-    Description: "have fun",
+    Name: "Game Night",
+    Description: "Have fun",
     Category : "Events",
-    Date: "Nov 22 6pm-10pm"
+    Date: "Nov 22 6pm-10pm",
+    Link: "https://discord.js.org/#/docs/main/stable/class/Client"
   }
-  var link = "https://discord.js.org/#/docs/main/stable/class/Client";
-  example.createnewchannel(message.guild, json, link);
+  example.createnewchannel(message.guild, json);
 };
 
 exports.help = {
--- example.js
+++ example.js
@@ -1,31 +1,39 @@
 module.exports = {
-  createnewchannel: function(guild, json, link) {
+  createnewchannel: function(guild, json) {
     return new Promise ( (res,rej) => {
       console.log('Create new channel called');
       //Grabbing variables from json. Declaring these variables so you can easily manage them if the json setup is different, which it wil be
+      //required fields: name, description, category, date
       var ch_name = json.Name;
       var ch_desc = json.Description;
-      var ch_cat = json.Category.;
-      // var date = json.Date
-      // var calendar_link = link;
-      var msg = "Date is " + json.Date.toString() + ".\n The link is "+ link.toString();
-      var event_id = '';
+      var ch_cat = json.Category.toLowerCase();
+      var ch_link = json.Link;
+      var ch_date = json.Date;
+      var msg = 'Event: ' + ch_name;
+      if (ch_date != undefined){
+        msg += '\nDate: ' + ch_date;
+      }
+      if (ch_link != undefined){
+        msg += '\nLink: ' + ch_link;
+      }
+        
+      // var msg = "Date is " + json.Date.toString() + ".\n The link is "+ ch_link.toString();
+      var cat_id = '';
       guild.channels.forEach(function(channel){
         if (channel.type == "category" && channel.deleted == false){
-          console.log(channel.name);
-          console.log(channel);
           if (channel.name == ch_cat){
-            event_id = channel.id;
+            cat_id = channel.id;
           }
         }
       });
+      if (cat_id == '')
       
       guild
         .createChannel(ch_name, { type: "text" })
         .then(async txtChannel => {
         console.log(`text channel created`);
           await txtChannel.edit({ topic: ch_desc });
-          await txtChannel.setParent(event_id)
+          await txtChannel.setParent(cat_id)
               .then(updated => console.log(`Set the category of ${updated.name} to ${updated.parent.name}`))
               .catch(console.error);
           await txtChannel.send(msg);
