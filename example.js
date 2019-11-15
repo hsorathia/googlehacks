934cd9f53c8d644d0f0d357d8ff4e93d282d93b4
Glitch (hello-express) <none>
2019-11-15 17:26:39 +0000

    🚉😋 Checkpoint ./example.js:2225217/2002 ./server.js:2827281/4177

---
+++ example.js
@@ -0,0 +1,18 @@
+module.exports = {
+  createnewchannel: function(guild, json) {
+    
+    //PLEASE CHANGE THESE LINES
+    //Grabbing variables from json. Declaring these variables so you can easily manage them if the json setup is different, which it wil be
+    var ch_name = json.channelname;
+    var ch_desc = json.channeldesc;
+    var calendar_link = json.callink;
+    
+    guild
+      .createChannel(ch_name, { type: "text" })
+      .then(async txtChannel => {
+        await txtChannel.edit({ topic: ch_desc });
+        await txtChannel.send(calendar_link);
+      })
+      .catch(console.error);
+  }
+};
