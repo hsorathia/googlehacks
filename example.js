a4b1edf5f8d8719510e32e69fd1cb71590e158b9
Jerry Lee <jerrylee1157@gmail.com>
2019-11-15 16:10:05 -0500

    Jerry (#1)
    * Initial Commit
    
    * 📰🌗 Checkpoint
    README.md:8/1
    
    * 📉🎎 Checkpoint
    ./views/index.html:8/774
    ./public/style.css:8/88
    
    * ✊🚙 Checkpoint
    ./public/style.css:8/80
    ./views/index.html:8/2108
    
    * 🚩💧 Checkpoint
    
    * 💶🙈 Checkpoint
    
    * 🏐👾 Checkpoint
    
    * 💫🍁 Checkpoint
    ./views/index.html:96831/224
    ./README.md:96831/18
    ./public/style.css:96831/282
    
    * 🏢🕌 Checkpoint
    ./public/vanilla-client.js:96831/1671
    ./public/client.js:96831/210
    ./views/index.html:96831/514
    
    * 🐥😎 Checkpoint
    ./views/index.html:96831/760
    ./public/vanilla-client.js:96831/1419
    ./public/style.css:96831/47
    
    * 🍕🌟 Checkpoint
    ./views/index.html:96831/368
    ./public/style.css:96831/36
    ./server.js:96831/437
    
    * 📕🌝 Checkpoint
    
    * 💅👵 Checkpoint
    ./package.json:96831/1389
    
    * 👡⛈ Checkpoint
    
    * 🚁🔔 Checkpoint
    
    * 🍜💩 Checkpoint
    ./.glitch-assets:658937/43
    
    * 👭👂 Checkpoint
    
    * 🌦🏏 Checkpoint
    ./public/client.js:96831/492
    
    * 🍚⚽️ Checkpoint
    
    * 💡⛱ Checkpoint
    
    * 🎬🍝 Checkpoint
    
    * 🚴🏯 Checkpoint
    ./views/index.html:334152/235
    ./public/client.js:334152/160
    
    * 📒🚨 Checkpoint
    ./package.json:728660/1
    
    * 🌔😺 Checkpoint
    ./server.js:334152/7
    
    * 👗🎓 Checkpoint
    ./server.js:334152/1
    
    * 💷🏮 Checkpoint
    
    * 🙋🎳 Checkpoint
    ./README.md:334152/29
    
    * 🐙💷 Checkpoint
    ./server.js:728660/16
    
    * 🚟🚪 Checkpoint
    ./server.js:728660/8
    
    * 🚔🍦 Checkpoint
    ./package.json:242011/2
    
    * 🎈🏞 Checkpoint
    ./server.js:617503/1313
    ./public/client.js:617503/2375
    
    * 🛢🏯 Checkpoint
    ./public/client.js:334152/36
    
    * 🚠📙 Checkpoint
    ./public/style.css:867163/33
    ./.glitch-assets:867163/86
    
    * 🚉😋 Checkpoint
    ./example.js:2225217/2002
    ./server.js:2827281/4177
    
    * 🎸🐧 Checkpoint
    ./example.js:2225217/1311
    ./server.js:2827281/219
    ./package.json:2225217/18:2827281/87
    ./bot.js:2225217/6822:2827281/110
    ./events/ready.js:2225217/2696
    ./commands/quit.js:2225217/256:2827281/1
    ./commands/ttc.js:2225217/358
    
    * 🔨☃️ Checkpoint
    ./commands/ttc.js:2225217/1436:2827281/132
    ./watch.json:2225217/243
    ./events/message.js:2225217/1169
    ./server.js:2225217/302
    ./bot.js:2225217/27
    ./package.json:2225217/32
    ./example.js:2225217/239:2827281/4
    
    * 🍢⚡️ Checkpoint
    ./events/message.js:2225217/58
    ./bot.js:2827281/208
    ./commands/ttc.js:2225217/3:2827281/191
    ./example.js:2225217/4
    
    * 🍨🙌 Checkpoint
    ./example.js:2827281/113
    ./commands/quit.js:2827281/2
    ./commands/ttc.js:2827281/5
    ./events/ready.js:2827281/1
    
    * 👬🐿 Checkpoint
    ./example.js:2827281/621
    
    * 🏀🍞 Checkpoint
    ./bot.js:2827281/7
    ./events/message.js:2827281/54
    ./example.js:2827281/285
    
    * 🌨🍙 Checkpoint
    ./example.js:2827281/956
    ./commands/ttc.js:2827281/25
    
    * 👘👨‍❤️‍👨 Checkpoint
    ./example.js:2827281/852
    ./commands/ttc.js:2827281/169
    
    * Edited some stuff
    
    * 👠👙 Checkpoint
    ./example.js:2827281/16
    ./bot.js:2827281/3
    
    * 💺🍍 Checkpoint
    
    * updated

---
+++ example.js
@@ -0,0 +1,45 @@
+module.exports = {
+  createnewchannel: function(guild, json) {
+    return new Promise ( (res,rej) => {
+      console.log('Create new channel called');
+      //Grabbing variables from json. Declaring these variables so you can easily manage them if the json setup is different, which it wil be
+      //required fields: name, description, category, date
+      var ch_name = json.Name;
+      var ch_desc = json.Description;
+      var ch_cat = json.Category.toLowerCase();
+      var ch_link = json.Link;
+      var ch_date = json.Date;
+      var msg = 'Event: ' + ch_name;
+      if (ch_date != undefined){
+        msg += '\nDate: ' + ch_date;
+      }
+      if (ch_link != undefined){
+        msg += '\nLink: ' + ch_link;
+      }
+        
+      // var msg = "Date is " + json.Date.toString() + ".\n The link is "+ ch_link.toString();
+      var cat_id = '';
+      guild.channels.forEach(function(channel){
+        if (channel.type == "category" && channel.deleted == false){
+          if (channel.name == ch_cat){
+            cat_id = channel.id;
+          }
+        }
+      });
+      
+      guild
+        .createChannel(ch_name, { type: "text" })
+        .then(async txtChannel => {
+        console.log(`text channel created`);
+          await txtChannel.edit({ topic: ch_desc });
+          await txtChannel.setParent(cat_id)
+              .then(updated => console.log(`Set the category of ${updated.name} to ${updated.parent.name}`))
+              .catch(console.error);
+          await txtChannel.send(msg);
+          res(txtChannel)
+        })
+        .catch(console.error);
+
+      });
+  }
+};
