ceeb71bc6e8cd11786c6afcde467471461977f5c
Peeradech <p.abhidechsakhulchai@gmail.com>
2019-11-15 16:55:50 -0800

    first try with old code

--- form_to_calendar.js
+++ form_to_calendar.js
@@ -1,3 +1,4 @@
+
 function onSubmit(e){
     var form = FormApp.getActiveForm();
     var formResponses = form.getResponses();
@@ -10,35 +11,89 @@ function onSubmit(e){
         answers[i] = wholeItems[i].getResponse();
     }
 
-    var addOrSearch = answers[0];
+    var addingMoreBool = answers[0];
     var title = answers[1];
     var emails = answers[2];
     var eventDate = answers[3];
     var eventTime = answers[4];
     var eventDuration = answers[5];
-    var id = answers[6];
 
-    if (addOrSearch === "Adding")
+    var testCalendar = CalendarApp.getDefaultCalendar();
+
+    
+    if (addingMoreBool === "New Event")
     {
+        console.log(addingMoreBool);
         var startTime = new Date(eventDate + "T" + eventTime + ":00Z");
         var endTime = new Date(startTime.getTime() + (eventDuration*60*60*1000));
-        var testCalendar = CalendarApp.getDefaultCalendar();
         var event = testCalendar.createEvent(title,
             startTime,
             endTime,
-            {
-                guests: emails,
-                sendInvites: true
-            }
-        );
-        form.setConfirmationMessage("ID is " + lastResponse.getId());
-
+                {
+                    guests: emails,
+                    sendInvites: true
+                }
+            );
     }
-    else if (addOrSearch === "Searching")
+    else
     {
-        for (var i = 0; i < formResponses; i++)
+        var items;
+        for (var i = 0; i < formResponses.length; i++)
         {
-            
+            var currentResponse = formResponses[i];
+            var searchedItems = currentResponse.getItemResponses();
+            if (title === searchedItems[1].getResponse())
+            {
+                if (eventDate === searchedItems[3].getResponse())
+                {
+                    if (eventTime === searchedItems[4].getResponse())
+                    {
+                        items = searchedItems;
+                        break;
+                    }
+                }
+            }
+        }
+        if (items != null )
+        {        
+            var searchedStartTime = new Date(items[3].getResponse() + "T" + items[4].getResponse() + ":00Z");
+            var searchedEndTime = new Date(searchedStartTime.getTime() + (items[5] * 60 * 60 * 1000));
+            if (searchedStartTime > searchedEndTime)
+            {
+                var searchedForEvent = testCalendar.getEvents(
+                    searchedStartTime, 
+                    searchedEndTime
+                    );
+            }
+            else
+            {
+                form.setConfirmationMessage("It doesn't find the event");
+            }
+            if (searchedForEvent !== null && searchedForEvent !== undefined)
+            {
+                if (emails !== null && searchedForEvent !== undefined)
+                {
+                    var separatedEmails = [];
+                    var emailAddress = "";
+                    for (var i = 0; i < emails.length; i++)
+                    {
+                        if (emails.charAt(i) === ";")
+                        {
+                            separatedEmails[separatedEmails.length] = emailAddress;
+                            emailAddress = "";
+                        }
+                        else
+                        {
+                            emailAddress += emails.charAt(i);
+                        }
+                    }
+                    for (var i = 0; i < separatedEmails.length; i++)
+                    {
+                        searchedForEvent.addGuest(separatedEmails[i]);
+                        console.log("Email " + i + ": " + separatedEmails[i]);
+                    }
+                }
+            }
         }
     }
 }
