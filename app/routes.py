a78cf3eb84655e3dd998629d75773252687aabdc
Peeradech <p.abhidechsakhulchai@gmail.com>
2019-11-15 18:12:35 -0800

    changed the db base

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from app.models import User
 
 @app.route('/')
 @app.route('/home')
@@ -10,7 +13,16 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    form = login()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for(""))
+
+        return '<h1>Invalid username or password</h1>'
     return render_template('login.html', form=form)
 
 
