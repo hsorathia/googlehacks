a7ec0e1b1741b9c8b965331523ab1a48f6e5a8d6
Habib <has.sorathia@gmail.com>
2019-11-15 17:58:31 -0800

    hotfix

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import InputRequired, Email, Length, DataRequired, EqualTo
+from wtforms.validators import InputRequired, Email, Length, DataRequired, EqualTo, ValidationError
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
-<<<<<<< HEAD
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
@@ -22,23 +21,11 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Login')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+        # user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
+        # user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
-=======
-
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('remember me')
-
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
->>>>>>> c265ae6fabc8340f8054e09a5a49e4c16094cee1
--- config.py
+++ config.py
@@ -1,9 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+import os
+basedir = os.path.abspath(os.path.dirname(__name__))
 
-class Config(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True, nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    image_file = db.Column(db.String(15), nullable=False,default='#jpg')
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-big-secret'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
